68b99afc1147999826435eee6e68c140754387db
PC <PC@DESKTOP-HOA8PKE>
2021-09-16 13:32:09 -0300

    Adicionar arquivos de projeto.

---
+++ ErickEspinosa.Singleton.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ErickEspinosa.Singleton.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singletonOne = Singleton.GetInstance();
+            var singletonTwo = Singleton.GetInstance();
+
+            if(singletonOne == singletonTwo)
+                Console.WriteLine("Funcionou, ambas instâncias são iguais");
+            else
+                Console.WriteLine("Não Funcionou, as instâncias são diferentes");
+        }
+    }
+}
---
+++ ErickEspinosa.Singleton.Console/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErickEspinosa.Singleton.ConsoleApp
+{
+    class Singleton
+    {
+        //Previne que outras classes chamem o operador "new"
+        private Singleton(){}
+
+        private static Singleton _singleton;
+
+        //Garante que sempre a mesma instância será retornada
+        public static Singleton GetInstance()
+        {
+            if (_singleton is null)
+                _singleton = new Singleton();
+
+            return _singleton;
+        }
+    }
+}
